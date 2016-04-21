b2f7366745fe4eb0f13f5ac0dad3e22be0f84357
Yahya Wessam <yahyawessam2002@gmail.com>
2016-04-21 19:41:39 +0300

    Ahhh, fuck it
    we must follow the rules...

--- turbo.mk
+++ turbo.mk
@@ -4,7 +4,6 @@ $(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
 $(call inherit-product, vendor/turbo/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/turbo/config/common.mk)
 $(call inherit-product, vendor/turbo/config/common_full_phone.mk)
 
 # ViPER4Android
